e7ec150d7d5b53658d9a934b8365ef409022414f
Balmukund Kumar <bkumarmukund@gmail.com>
2024-10-02 14:55:59 +0530

    chapter 6
    easiest way of writing one to many relationship
    using @ManyToOne

---
+++ laurSpilcaYT/jpa_2023_c6_e1/src/main/java/com/freemyip/c0de/Main.java
@@ -0,0 +1,67 @@
+package com.freemyip.c0de;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import com.freemyip.c0de.entities.Comment;
+import com.freemyip.c0de.entities.Post;
+import com.freemyip.c0de.persistence.CustomPersistenceUnitInfo;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, String> props = new HashMap<>();
+
+        props.put("hibernate.show_sql", "true");
+        props.put("hibernate.hbm2ddl.auto", "create");
+
+        // grab data from database
+
+        // persistence using programatic approach no xml
+        EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(
+                        new CustomPersistenceUnitInfo(), props);
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            
+            // create a post 
+            Post post = new Post();
+            post.setTitle("ndtv owner changed");
+            post.setContent("andani became the new owner!");
+
+            Comment c1 = new Comment();
+            c1.setPost(post);
+            c1.setContent("not a very good news");
+            
+            Comment c2 = new Comment();
+            c2.setPost(post);
+            c2.setContent("ndtv is now managed by non media person");
+            
+            Comment c3 = new Comment();
+            c3.setPost(post);
+            c3.setContent("ndtv now on path of other media houses");
+            
+
+            em.persist(c3);
+            em.persist(c2);
+            em.persist(c1);
+
+            em.persist(post);
+
+
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("error connecting to DB: " + e.getMessage());
+        } finally {
+            em.close();
+        }
+    }
+}
---
+++ laurSpilcaYT/jpa_2023_c6_e1/src/main/java/com/freemyip/c0de/entities/Comment.java
@@ -0,0 +1,46 @@
+package com.freemyip.c0de.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Comment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String content;
+
+    @ManyToOne
+    private Post post;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+
+}
