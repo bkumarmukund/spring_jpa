b34fe64d5595b83838c41d87bd2a1db456cdab66
Balmukund Kumar <bkumarmukund@gmail.com>
2024-10-02 23:33:14 +0530

    chapter 8
    Entity Inheritance
    
    using table per class strategy

--- laurSpilcaYT/jpa_2023_c8_e1/src/main/java/com/freemyip/c0de/entities/Product.java
+++ laurSpilcaYT/jpa_2023_c8_e1/src/main/java/com/freemyip/c0de/entities/Product.java
@@ -6,9 +6,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.MappedSuperclass;
 
-@Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Product {
     
     @Id
